5675fbd79e8e229e9db423ee475a2e0cc4c63281
karynntran <karynn.tran@gmail.com>
2015-12-20 00:31:14 -0500

    Loading node app through gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,52 +3,70 @@ var gulp = require('gulp'),
 	connect = require('gulp-connect'),
 	livereload = require('gulp-livereload'),
 	jade = require('gulp-jade'),
+	exec = require('child_process').exec,
+	tinylr = require('tiny-lr'),
+	server = tinylr(),
 	nodemon = require('nodemon');
 
-gulp.task('server', function(){
-	connect.server({
-		root: [__dirname],
-		port: 8080,
-		livereload: true
-	});
-});
-
-gulp.task('jade', function() {
-    return gulp.src('views/*.jade')
-        .pipe(jade()) // pip to jade plugin
-        .pipe(gulp.dest(__dirname)); // tell gulp our output folder
-});
-
-gulp.task('start', function () {
-  nodemon({
-    script: 'server.js'
+gulp.task('server', function (cb) {
+  exec('npm start', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
   })
-})
+});
+
+// gulp.task('server', function(){
+// 	connect.server({
+// 		root: [__dirname],
+// 		port: 8080,
+// 		livereload: true
+// 	});
+// });
+
+// gulp.task('jade', function() {
+//     return gulp.src('views/index.jade')
+//         .pipe(jade()) // pip to jade plugin
+//         .pipe(gulp.dest(__dirname)); // tell gulp our output folder
+// });
+
+// gulp.task('start', function () {
+//   nodemon({
+//     script: 'server.js'
+//   })
+// })
 
 gulp.task('styles', function(){
-	gulp.src('public/stylesheets/sass/**/*.scss')
+	gulp.src('/stylesheets/sass/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('public/stylesheets/css/'))
-        .pipe(connect.reload());
+        .pipe( livereload( server ));
 });
 
 gulp.task('js', function(){
-	gulp.src('javascripts/*.js')
-		.pipe(connect.reload());
+	gulp.src('/javascripts/*.js')
+		.pipe( livereload( server ));
 })
 
-gulp.task('index', function(){
-	gulp.src('views/index.jade')
-		.pipe(connect.reload());
+gulp.task('jade', function(){
+	gulp.src('/views/*.jade')
+		.pipe( livereload( server ));
 })
 
 gulp.task('watch', function(){
-	gulp.watch('public/javascripts/*.js', ['js']);
-	gulp.watch('public/stylesheets/sass/**/*.scss', ['styles']);
-	gulp.watch('views/index.jade', ['index']);
+    server.listen(35729, function (err) {
+	    if (err) {
+	      return console.log(err);
+	    }
+
+    gulp.watch('/public/javascripts/*.js', ['js']);
+	gulp.watch('/public/stylesheets/sass/**/*.scss', ['styles']);
+	gulp.watch('/views/index.jade', ['jade']);
+
+	});
 });
 
 
-gulp.task('default',['server', 'start', 'watch', 'jade'], function(){
+gulp.task('default',['server', 'styles', 'js', 'jade', 'watch'], function(){
 
 });
