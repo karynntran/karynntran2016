f8be9fd2adc9c04cc8e1a4998ef2b18c81d2fafa
karynntran <karynn.tran@gmail.com>
2016-01-19 21:06:26 -0500

    Updates post deploying on Heroku

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,3 +1,11 @@
+var categories = ["home", "about", "portfolio", "experience", "contact"]
+
 document.addEventListener("DOMContentLoaded", function (){
 	$('#intro').addClass('animate');
+
+	for (var i in categories) {
+	  	$('#'+categories[i]).click(function(){
+	  		console.log(categories[i])
+	  	});
+	}
 })
